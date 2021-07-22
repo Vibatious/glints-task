af5bd66dcb8b568d96e7510a8c6bd0f9f81db44f
vibatious <varshneylove1998@gmail.com>
2021-07-23 03:14:53 +0530

    ADD: Server Endpoint and heartbeat enpoint for server checkup

--- server/src/app-server/app-server.ts
+++ server/src/app-server/app-server.ts
@@ -5,10 +5,95 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const router = express.Router();
+app.get('/heartbeat', (req, res) => {
+	try {
+		res.send('❤️❤️❤️❤️❤️');
+	} catch (error) {
+		res.status(503).send('💔💔');
+	}
+});
 
-app.use(router);
+app.get('/get-opened-resturants', async (req, res) => {
+	const dateTime = req.query.dateTime;
+	try {
+		if (dateTime) {
 
+		} else {
+			res.status(400).send({ error: 'In sufficient Information' });
+		}
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+})
+
+app.get('/get-top-resturants', async (req, res) => {
+	const { top, greaterThan, lessThan, fromPrice, toPrice } = req.query;
+	try {
+		if (top && fromPrice && toPrice && (greaterThan || lessThan)) {
+			if (greaterThan) {
+
+			} else {
+
+			}
+		} else {
+			res.status(400).send({ error: 'In sufficient Information' });
+		}
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+})
+
+app.get('/relevant-resturants', async (req, res) => {
+	const { searchTerm } = req.query;
+	try {
+		if (searchTerm) {
+
+		} else {
+			res.status(400).send({ error: 'In sufficient Information' });
+		}
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+})
+
+app.get('/relevant-dishes', async (req, res) => {
+	const { searchTerm } = req.query;
+	try {
+		if (searchTerm) {
+
+		} else {
+			res.status(400).send({ error: 'In sufficient Information' });
+		}
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+})
+
+app.get('/relevant-dishes', async (req, res) => {
+	const { searchTerm } = req.query;
+	try {
+		if (searchTerm) {
+
+		} else {
+			res.status(400).send({ error: 'In sufficient Information' });
+		}
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+})
+
+app.get('/place-order/userid/:uid/resturantid/:rid/dishid/:did', async (req, res) => {
+	const { uid, rid, did } = req.query;
+	try {
+		if (uid && rid && did) {
+
+		} else {
+			res.status(400).send({ error: 'In sufficient Information' });
+		}
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+})
 
 try {
 	const server = app.listen(environment.port, () => {
