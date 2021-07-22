fa9c3c3fbefbef14b7cb7fc1d716b299a060ee4a
vibatious <varshneylove1998@gmail.com>
2021-07-23 03:15:33 +0530

    ADD: sql configuration

--- server/src/environment.ts
+++ server/src/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
-	port: 3000
+	port: 3000,
+	sqlDatabase: {
+		host: 'localhost',
+		user: 'root',
+		port: 11007,
+		// password: 'data',
+		// database: 'database-name',
+	},
 }
